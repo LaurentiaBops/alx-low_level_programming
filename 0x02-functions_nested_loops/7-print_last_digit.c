a140593dd26851c12cf8ffab6341c5386eda1331
LaurentiaBops <bopela.nb@gmail.com>
2023-03-22 19:02:07 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Display last number
+ * @n : Input variable
+ * Return: Last digit of number
+ */
+
+int print_last_digit(int n)
+{
+
+	int last;
+	last = n % 10;
+
+	if (last <0)
+	{
+		last = last * -1;
+	}
+
+	_putchar(last + '0');
+	return (last);
+}
