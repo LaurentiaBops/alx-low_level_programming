bf7d5b5ec3d3187283e88a6aaa2a899a0ea0fea6
LaurentiaBops <bopela.nb@gmail.com>
2023-03-23 13:34:37 -0700

    Print numbers followed by line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Print numbers (0 to 9) followed by line
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
