e6aa2ed06efe8e2b1b365c51d05a1f40695ec85f
LaurentiaBops <bopela.nb@gmail.com>
2023-03-27 10:46:11 -0700

    Reset pointer to 98

---
+++ 0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Updates value pointer points to 98
+ * @n = Pointer 
+ * Return nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
