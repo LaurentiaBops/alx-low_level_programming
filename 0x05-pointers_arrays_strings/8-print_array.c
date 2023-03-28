f2ab5d8b2697a590ec5cf59c70ce158a32e05441
LaurentiaBops <bopela.nb@gmail.com>
2023-03-28 13:12:06 -0700

    Print array with commas

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Print n elements of array followed by line
+ * 
+ * @a: an input array
+ * @n: an input integer
+ *
+ * Return: Nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+	}
+	putchar('\n');
+}
