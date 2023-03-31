8df1b218bf1655e6852056d5b985840594a6f265
LaurentiaBops <bopela.nb@gmail.com>
2023-03-30 17:12:34 -0700

    Rotate characters by 13 places

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,17 +8,17 @@
 
 char *rot13(char *Str)
 {
-	char s[26] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w'
+	char s[29] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w'
 		, 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
 		'h', 'i', 'j', 'k', 'l', 'm', 'E', 'B', 'G'};
-	char o[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'
+	char o[29] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'
 		, 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
 		'v', 'w', 'x', 'y', 'z', 'R', 'O', 'T'};
 	int i = 0, j;
 
 	while (Str[i])
 	{
-		for (j = 0; j < 26; j++)
+		for (j = 0; j < 29; j++)
 		{
 			if (Str[i] == o[j])
 			{
