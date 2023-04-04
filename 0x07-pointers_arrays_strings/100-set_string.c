8d0b12b3b5642e87d8450e4a99d558dede10503c
LaurentiaBops <bopela.nb@gmail.com>
2023-04-04 15:39:05 -0700

    Sets values of printer to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - set value of printer to char
+ * @s: Pointer
+ * @to: input character
+ * Return: Nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
