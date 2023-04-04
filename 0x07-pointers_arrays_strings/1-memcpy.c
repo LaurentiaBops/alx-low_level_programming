3e08d81308049e057b770f00ede0db2330d7944d
LaurentiaBops <bopela.nb@gmail.com>
2023-04-03 20:37:20 -0700

    starting off

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies from one memory location to another
+ * @src: location to be copied from
+ * @dest: location being copied to
+ * @n: input bytes being copied
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+}
